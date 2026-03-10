3044841270c35ca4c3f8174ebe924053690c49fe
SuneethaNaidu <suneethanaidu137@gmail.com>
2026-03-10 12:22:37 +0530

    Update predict_disaster.py

--- prediction/predict_disaster.py
+++ prediction/predict_disaster.py
@@ -1,14 +1,11 @@
 import joblib
 import numpy as np
 import os
-import joblib
-import numpy as np
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model_path = os.path.join(BASE_DIR, "models", "disaster_model.pkl")
 
 model = joblib.load(model_path)
-model = joblib.load("models/disaster_model.pkl")
 
 def predict_disaster(rainfall,temperature,humidity,wind):
 
