62f77ec86cee3057f4f68f4c84aba47a2b2bc3ae
SuneethaNaidu <suneethanaidu137@gmail.com>
2026-03-10 12:39:01 +0530

    Update predict_disaster.py

--- prediction/predict_disaster.py
+++ prediction/predict_disaster.py
@@ -1,19 +1,17 @@
+import os
 import joblib
 import numpy as np
-import os
 
+# Get project root directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Correct model path
 model_path = os.path.join(BASE_DIR, "models", "disaster_model.pkl")
 
+# Load model
 model = joblib.load(model_path)
 
-def predict_disaster(rainfall,temperature,humidity,wind):
-
-    data = np.array([[rainfall,temperature,humidity,wind]])
-
-    result = model.predict(data)
-
-    if result == 1:
-        return "HIGH DISASTER RISK"
-    else:
-        return "SAFE"
+def predict_disaster(rainfall, temperature, humidity, wind):
+    data = np.array([[rainfall, temperature, humidity, wind]])
+    prediction = model.predict(data)
+    return prediction[0]
